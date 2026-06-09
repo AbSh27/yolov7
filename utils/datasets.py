b09384ba17bc8718ab6591ca53f3da2d0e16c9c6
AbSh27 <abdullahshareef78@gmail.com>
2026-06-10 01:45:25 +0530

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -684,7 +684,7 @@ def load_image(self, index):
 
 
 def augment_hsv(img, hgain=0.5, sgain=0.5, vgain=0.5):
-    rgb = img[:,:,;3]
+    rgb = img[:,:,:3]
     r = np.random.uniform(-1, 1, 3) * [hgain, sgain, vgain] + 1  # random gains
     hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
     dtype = img.dtype  # uint8
